34c0f1d52ea4e4681322db890c4b6c786b944dbf
asmith <mrpresidentsmith@gmail.com>
2019-04-16 19:04:13 -0700

    Added ShipControl script.  Implemented ship rotation.  Working on thrust application.

---
+++ FinalProject/Assets/Scripts/ShipControl.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipControl : MonoBehaviour
+{
+    [SerializeField]
+    float rotationSpeed = 90.0f;
+    [SerializeField]
+    Rigidbody2D rb2d;
+    [SerializeField]
+    float thrust = 0.2f;
+
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float input = Input.GetAxis("Horizontal");
+        
+        //Rotate
+        if (input != 0)
+        {
+            Vector3 rotationForShip = new Vector3(0, 0, input);
+            rotationForShip *= -rotationSpeed * Time.deltaTime;
+            transform.Rotate(rotationForShip,Space.Self);
+        }
+
+        //Apply thrust
+        if (Input.GetKey(KeyCode.Space))
+        {
+
+            Vector2 forwardVector = new Vector2(-1,1);
+            rb2d.AddForce(forwardVector * thrust);
+        }
+        
+    }
+
+    
+
+}
+
+/*
+ * // Smoothly tilts a transform towards a target rotation.
+        
+
+        // Rotate the cube by converting the angles into a quaternion.
+        Quaternion target = Quaternion.Euler(tiltAroundX, 0, tiltAroundZ);
+
+       
+*/

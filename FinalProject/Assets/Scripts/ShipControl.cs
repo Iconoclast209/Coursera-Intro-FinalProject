71c7d04b2144de0484f572f84262cedbeb896902
asmith <mrpresidentsmith@gmail.com>
2019-04-18 16:56:35 -0700

    Completed project.  Finished screen wrapping.

--- FinalProject/Assets/Scripts/ShipControl.cs
+++ FinalProject/Assets/Scripts/ShipControl.cs
@@ -35,7 +35,6 @@ public class ShipControl : MonoBehaviour
         //Apply thrust
         if (Input.GetKey(KeyCode.Space))
         {
-
             Vector2 forwardVector = new Vector2(0,1);
             rb2d.AddRelativeForce(forwardVector * thrust * Time.deltaTime, ForceMode2D.Impulse);
         }
@@ -44,15 +43,43 @@ public class ShipControl : MonoBehaviour
 
     private void OnBecameInvisible()
     {
-           //Find position on screen
-        if((transform.position.x + colliderRadius) > ScreenUtils.ScreenRight)
+        //Find current position
+        Vector2 newPosition = transform.position;
+        
+        //Determine what edge of the screen the ship has exited
+        if((newPosition.x + colliderRadius) > ScreenUtils.ScreenRight)
         {
             Debug.Log("Ship has exited screen right.");
+            newPosition.x *= -1;
+            /*if(newPosition.y > ScreenUtils.ScreenTop)
+            {
+                newPosition.y = ScreenUtils.ScreenTop - 0.001f;
+            }*/
         }
-        else if ((transform.position.x - colliderRadius) < ScreenUtils.ScreenLeft)
+        else if ((newPosition.x - colliderRadius) < ScreenUtils.ScreenLeft)
         {
             Debug.Log("Ship has exited screen left.");
+            newPosition.x *= -1;
+            if (newPosition.y > ScreenUtils.ScreenTop)
+            {
+                newPosition.y = ScreenUtils.ScreenTop - 0.001f;
+            }
         }
+
+        if ((newPosition.y - colliderRadius) < ScreenUtils.ScreenBottom)
+        {
+            Debug.Log("Ship has exited screen bottom.");
+            newPosition.y *= -1;
+        }
+        else if ((newPosition.y + colliderRadius) > ScreenUtils.ScreenTop)
+        {
+            Debug.Log("Ship has exited screen top.");
+            newPosition.y *= -1;
+        }
+
+        //Adjust the ship's position to the opposite side of the screen
+        transform.position = newPosition;
+
     }
 
 

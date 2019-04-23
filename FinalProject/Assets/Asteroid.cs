d8c92d123c2f3110027b1df96ed3b1a12b5ce646
asmith <mrpresidentsmith@gmail.com>
2019-04-22 17:45:43 -0700

    Added Course Two - Week One Project content.  Added asteroid and empty asteroid script.

---
+++ FinalProject/Assets/Asteroid.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    /*  
+    Include a moving asteroid in your game that starts moving in a random direction with one of 3 random sprites
+    Screen wrap the asteroid. For example, when the asteroid leaves the bottom of the game window it should re-appear 
+    at the top of the game window
+    */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Select a random sprite
+        //Move in a random direction
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}

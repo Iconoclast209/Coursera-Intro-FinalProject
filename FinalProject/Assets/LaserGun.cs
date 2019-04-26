746d26afbb6681a1bd78222f40f222dabde835d0
asmith <mrpresidentsmith@gmail.com>
2019-04-26 08:00:15 -0700

    Implemented laser bolts to be fired from front of ship.  Still need to destroy bolts when they leave the play area.  Need to set bolts to interact with asteroids, and asteroids to destroy ship.

---
+++ FinalProject/Assets/LaserGun.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserGun : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject laserBolt;
+    [SerializeField]
+    float thrust = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 laserVector = new Vector3(transform.position.x, transform.position.y,0f);
+            //GameObject currentLaserBolt = Instantiate(laserBolt, laserVector, Quaternion.identity);
+            Quaternion laserQuaternion = this.transform.rotation;
+            GameObject currentLaserBolt = Instantiate(laserBolt, laserVector, laserQuaternion);
+            currentLaserBolt.GetComponent<Rigidbody2D>().AddRelativeForce(Vector2.up * thrust, ForceMode2D.Impulse);
+        }
+    }
+}
